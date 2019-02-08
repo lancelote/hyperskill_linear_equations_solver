ca31a0ad16f0759a7d209eb9b626c9155b0f9fae
Pavel Karateev <lancelote.du.lac@gmail.com>
2019-02-08 20:03:06 +0300

    Stage 3 Assignment 35

---
+++ src/stage03/hw35/Main.java
@@ -0,0 +1,46 @@
+package stage03.hw35;
+
+import java.util.Scanner;
+
+/**
+ * Given a rectangle array n×m in size. Rotate it by 90 degrees clockwise, by recording the result into the new array
+ * m×n in size.
+ */
+public class Main {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int rows = scanner.nextInt();
+        int cols = scanner.nextInt();
+        int[][] matrix = new int[rows][cols];
+
+        for (int i = 0; i < rows; i++) {
+            for (int j = 0; j < cols; j++) {
+                matrix[i][j] = scanner.nextInt();
+            }
+        }
+
+        print(rotate(matrix));
+    }
+
+    private static void print(int[][] matrix) {
+        for (int[] row : matrix) {
+            for (int item : row) {
+                System.out.print(item + " ");
+            }
+            System.out.println();
+        }
+    }
+
+    private static int[][] rotate(int[][] matrix) {
+        int rows = matrix.length;
+        int cols = matrix[0].length;
+        int[][] rotated = new int[cols][rows];
+
+        for (int i = 0; i < rows; i++) {
+            for (int j = 0; j < cols; j++) {
+                rotated[j][rows - 1 - i] = matrix[i][j];
+            }
+        }
+        return rotated;
+    }
+}
