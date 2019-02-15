037869b772fc8346f79914702cd576cebd015a4b
Pavel Karateev <lancelote.du.lac@gmail.com>
2019-02-15 14:13:40 +0300

    Stage 3 Assignment 48

---
+++ src/stage03/hw48/Main.java
@@ -0,0 +1,26 @@
+package stage03.hw48;
+
+public class Main {
+}
+
+class Complex {
+
+    private double real;
+    private double image;
+
+    public double getReal() {
+        return real;
+    }
+
+    public void setReal(double real) {
+        this.real = real;
+    }
+
+    public double getImage() {
+        return image;
+    }
+
+    public void setImage(double image) {
+        this.image = image;
+    }
+}
