bd86e4b25dda87d1f4a0daeab30d8fd9a961e85a
Pavel Karateev <lancelote.du.lac@gmail.com>
2019-02-17 12:53:33 +0300

    Stage 3 Assignment 50

---
+++ src/stage03/hw50/Main.java
@@ -0,0 +1,35 @@
+package stage03.hw50;
+
+public class Main {
+}
+
+class Book {
+
+    private String title;
+    private int yearOfPublishing;
+    private String[] authors;
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public int getYearOfPublishing() {
+        return yearOfPublishing;
+    }
+
+    public void setYearOfPublishing(int yearOfPublishing) {
+        this.yearOfPublishing = yearOfPublishing;
+    }
+
+    public String[] getAuthors() {
+        return authors;
+    }
+
+    public void setAuthors(String[] authors) {
+        this.authors = authors;
+    }
+}
