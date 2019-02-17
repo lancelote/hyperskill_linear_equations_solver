40fcc32348f9475515e84ca8af1e197bdce5eafb
Pavel Karateev <lancelote.du.lac@gmail.com>
2019-02-17 12:50:24 +0300

    Stage 3 Assignment 49

---
+++ src/stage03/hw49/Main.java
@@ -0,0 +1,35 @@
+package stage03.hw49;
+
+public class Main {
+}
+
+class Account {
+
+    private long balance;
+    private String ownerName;
+    private boolean locked;
+
+    public long getBalance() {
+        return balance;
+    }
+
+    public void setBalance(long balance) {
+        this.balance = balance;
+    }
+
+    public String getOwnerName() {
+        return ownerName;
+    }
+
+    public void setOwnerName(String ownerName) {
+        this.ownerName = ownerName;
+    }
+
+    public boolean isLocked() {
+        return locked;
+    }
+
+    public void setLocked(boolean locked) {
+        this.locked = locked;
+    }
+}
